9a96907739ed2ba1c8519ef1baf4425f488dcfd5
Robin Norwood <rnorwood@hashicorp.com>
2025-07-29 14:48:16 -0500

    Initial azure config

---
+++ azure/main.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "terraform-provider-example"
+  location = "West US 2"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "terraformproviderexample"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
