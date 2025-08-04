72e45095e7809e50fa8f13e4835654a83ba702bb
Robin Norwood <rnorwood@hashicorp.com>
2025-08-04 09:42:38 -0500

    No hyphens

--- azure/main.tf
+++ azure/main.tf
@@ -16,7 +16,7 @@ resource "random_string" "suffix" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "learn-${random_string.suffix.result}"
+  name                     = "learn${random_string.suffix.result}"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
