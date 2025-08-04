5fb261ef794767d86b2160b22812728821aee847
Robin Norwood <rnorwood@hashicorp.com>
2025-08-04 09:57:14 -0500

    Randomize GCP bucket name as well

--- gcp/main.tf
+++ gcp/main.tf
@@ -2,8 +2,16 @@ provider "google" {
   region = "us-west1"
 }
 
+provider "random" { }
+
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
 resource "google_storage_bucket" "example" {
-  name     = "terraform-provider-example"
+  name     = "terraform-provider-${random_string.suffix.result}"
   location = "US"
 
   uniform_bucket_level_access = true
--- gcp/terraform.tf
+++ gcp/terraform.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 6.45.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7.2"
+    }
   }
   required_version = ">= 1.2"
 }
