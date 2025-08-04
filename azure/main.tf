dfe7053416428bebc87b9a8a15f6f754e76c4594
Robin Norwood <rnorwood@hashicorp.com>
2025-08-04 08:41:17 -0500

    Fix unique name issue

--- azure/main.tf
+++ azure/main.tf
@@ -7,8 +7,14 @@ resource "azurerm_resource_group" "example" {
   location = "West US 2"
 }
 
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
 resource "azurerm_storage_account" "example" {
-  name                     = "terraformproviderexample"
+  name                     = "learn-${random_string.suffix.result}"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
--- azure/terraform.tf
+++ azure/terraform.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.36.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7.2"
+    }
   }
   required_version = ">= 1.2"
 }
